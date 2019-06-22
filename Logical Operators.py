56ee5f60152a859f4b1f9a21fae03cb28d409e5f
mahema1 <51996388+mahema1@users.noreply.github.com>
2019-06-22 21:52:08 +0530

    Add files via upload

---
+++ Logical Operators.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=10
+res=(x<15) and (x<8)
+print(res)
+
+
+# In[2]:
+
+
+x=10
+res=(x>9) and (x<14)
+print(res)
+
+
+# In[3]:
+
+
+x=10
+res=(x>1) or (x>20)
+print(res)
+
+
+# In[4]:
+
+
+x=10
+res=(x>100) or (x<20)
+print(res)
+
+
+# In[6]:
+
+
+x=False
+res= not(x)
+print(res)
+
+
+# In[ ]:
+
+
+
+
